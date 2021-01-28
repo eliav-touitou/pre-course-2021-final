95f667e8531254a8c513e7abb766c6168cd18fbf
eliav-touitou <77244738+eliav-touitou@users.noreply.github.com>
2021-01-28 18:00:43 +0200

    fixing local storage
    creating a new function to be able to use local storage

--- src/main.js
+++ src/main.js
@@ -1,37 +1,62 @@
-// creating variables
-const addButton = document.querySelector("#add-button");
-const viewSection = document.querySelector("#view-section");
-let counter = 0;
+document.addEventListener("DOMContentLoaded", () => {
+  // creating variables
 
-//event listeners
-addButton.addEventListener("click", addTodo);
-addButton.addEventListener("click", taskCounter);
+  let tasks = JSON.parse(localStorage.getItem("my-todo"));
+  if (!tasks) {
+    tasks = [];
+  }
+  const addButton = document.querySelector("#add-button");
+  const viewSection = document.querySelector("#view-section");
+  const sortButton = document.querySelector("#sort-button");
+  for (const task of tasks) {
+    viewSection.append(createContainer(task));
+  }
 
-//functions
-function addTodo(e) {
-  e.preventDefault();
-  //  generating the container of the new task
-  const todoContainer = document.createElement("div");
-  todoContainer.className = "todo-container";
-  viewSection.append(todoContainer);
-  //  generating the priority div of the new task
-  const todoPriority = document.createElement("div");
-  todoPriority.innerText = document.querySelector("#priority-selector").value;
-  todoPriority.className = "todo-priority";
-  todoContainer.append(todoPriority);
-  //  generating the time div of the new task
-  const createdAt = document.createElement("div");
-  createdAt.innerText = new Date().toISOString().slice(0, 19).replace("T", " ");
-  createdAt.className = "todo-created-at";
-  todoContainer.append(createdAt);
-  //  generating the text div of the new task
-  const todoText = document.createElement("div");
-  todoText.innerText = document.querySelector("#text-input").value;
-  todoText.className = "todo-text";
-  todoContainer.append(todoText);
-  document.querySelector("#text-input").value = "";
-}
+  //event listeners
 
-function taskCounter() {
-  document.getElementById("counter").innerHTML = ++counter + "tasks on list";
-}
+  addButton.addEventListener("click", addTodo);
+  //functions
+  function addTodo() {
+    let task = {
+      text: document.querySelector("#text-input").value,
+      priority: document.querySelector("#priority-selector").value,
+      date: new Date().getTime(),
+    };
+    viewSection.append(createContainer(task));
+    tasks.push(task);
+    localStorage.setItem("my-todo", JSON.stringify(tasks));
+    document.querySelector("#text-input").value = "";
+  }
+
+  function createContainer(task) {
+    //  generating the container of the new task
+    const todoContainer = document.createElement("div");
+    todoContainer.className = "todo-container";
+    //  generating the priority div of the new task
+    const todoPriority = document.createElement("div");
+    todoPriority.className = "todo-priority";
+    todoPriority.innerText = task.priority;
+    todoContainer.append(todoPriority);
+    //  generating the time div of the new task
+    const createdAt = document.createElement("div");
+    createdAt.innerText = new Date(task.date)
+      .toISOString()
+      .slice(0, 19)
+      .replace("T", " ");
+    createdAt.className = "todo-created-at";
+    todoContainer.append(createdAt);
+    //  generating the text div of the new task
+    const todoText = document.createElement("div");
+    todoText.innerText = task.text;
+    todoText.className = "todo-text";
+    todoContainer.append(todoText);
+    // saveLocalTodos(todoContainer);
+    return todoContainer;
+  }
+
+  //
+  // function taskCounter() {
+  //     ++counter;
+  //     document.getElementById("counter").innerHTML = counter + "tasks on list";
+  //   }
+});
