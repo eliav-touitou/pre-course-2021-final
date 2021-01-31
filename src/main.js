ecac29a924d0aff73dcce3e0ebc5399c02ebcc1b
eliav-touitou <77244738+eliav-touitou@users.noreply.github.com>
2021-01-31 23:45:22 +0200

    fixing codes comments

--- src/main.js
+++ src/main.js
@@ -2,19 +2,11 @@
 
 document.addEventListener("DOMContentLoaded", async () => {
   // creating variables
-
   const addButton = document.querySelector("#add-button");
   const viewSection = document.querySelector("#view-section");
   const sortButton = document.querySelector("#sort-button");
   const counter = document.querySelector("#counter");
   // getting the array out of the storage and creating container for each object of the array
-  // let tasks = JSON.parse(localStorage.getItem("my-todo"));
-  // if (!tasks) {
-  //   tasks = [];
-  // }
-  // for (const task of tasks) {
-  //   viewSection.append(createContainer(task));
-  // }
   let tasks = await getPersistent();
   if (!tasks) {
     tasks = [];
@@ -25,12 +17,12 @@ document.addEventListener("DOMContentLoaded", async () => {
   //calling the showTask function for updating the tasks counter on loading
   showTasks();
 
-  //event listeners
+  //event listeners -
   addButton.addEventListener("click", addTodo);
   sortButton.addEventListener("click", sortTasks);
   viewSection.addEventListener("click", doneTask);
 
-  //functions
+  //functions -
 
   //function for creating new task as an arrays object
   function addTodo() {
@@ -40,14 +32,15 @@ document.addEventListener("DOMContentLoaded", async () => {
       date: new Date().getTime(),
     };
     console.log(task);
-
+    //inserting the data to a container for displaying it properly on the pge
     viewSection.append(createContainer(task));
     tasks.push(task);
-    // localStorage.setItem("my-todo", JSON.stringify(tasks));
     setPersistent(tasks);
     showTasks();
+    //emptying the input for the next task todo
     document.querySelector("#text-input").value = "";
   }
+
   //function for generating the html container for the "task" object
   function createContainer(task) {
     //  generating the container of the new task
@@ -66,7 +59,7 @@ document.addEventListener("DOMContentLoaded", async () => {
     todoPriority.innerText = task.priority;
     todoContainer.append(todoPriority);
 
-    //  generating the time div of the new task
+    //  generating the time div of the new task and displaying it in SQL format
     const createdAt = document.createElement("div");
     createdAt.innerText = new Date(task.date)
       .toISOString()
@@ -74,12 +67,12 @@ document.addEventListener("DOMContentLoaded", async () => {
       .replace("T", " ");
     createdAt.className = "todo-created-at";
     todoContainer.append(createdAt);
-    //create complete button
+    //create complete button and assigning classes for decoration
     const completedButton = document.createElement("button");
     completedButton.innerHTML = `<i class="fas fa-check"></i>`;
     completedButton.classList.add("complete-button");
     todoContainer.appendChild(completedButton);
-    //Create trash button
+    //Create trash button and assigning classes for decoration
     const trashButton = document.createElement("button");
     trashButton.innerHTML = `<i class="fas fa-trash"></i>`;
     trashButton.classList.add("trash-button");
@@ -88,11 +81,12 @@ document.addEventListener("DOMContentLoaded", async () => {
     return todoContainer;
   }
 
-  // function for counting tasks
+  // function for counting tasks and displaying the amount in the counter
   function showTasks() {
     counter.textContent = tasks.length;
     console.log(tasks.length);
   }
+
   // function for sorting tasks by their priority
   function sortTasks() {
     tasks = tasks.sort((a, b) => b.priority - a.priority);
@@ -102,35 +96,36 @@ document.addEventListener("DOMContentLoaded", async () => {
       viewSection.append(container);
     }
   }
-  //function for checking a done task by parent element of the clicked button
+
+  //function for assigning new class to the todo-container for animating it due to the clicked button(trash/complete)
   function doneTask(e) {
     const item = e.target;
-
+    //when the trash button is clicked the code below will be executed to animate the container and then delete it
     if (item.classList[0] === "trash-button") {
-      const task = item.parentElement;
+      const taskContainer = item.parentElement;
       if (!confirm(`Are you sure you want to delete task?`)) {
         return;
       }
-      //assigning a new class for fall animation, and delete only after
-      task.classList.add("fall");
-      task.addEventListener("transitionend", (e) => {
-        removeLocalTodos(task);
+      taskContainer.classList.add("fall");
+      taskContainer.addEventListener("transitionend", (e) => {
+        removeLocalTodos(taskContainer);
       });
     }
+    //when the complete button is clicked the code below will be executed to animate the container
+
     if (item.classList[0] === "complete-button") {
-      const task = item.parentElement;
-      task.classList.toggle("completed");
+      const taskContainer = item.parentElement;
+      taskContainer.classList.toggle("completed");
     }
   }
-  // function for removing the deleted task  out of the local storage array
-  function removeLocalTodos(task) {
+
+  // function for removing the deleted task  out of the storage
+  function removeLocalTodos(taskContainer) {
     const containerNodeList = document.querySelectorAll(".todo-container");
     const containerArray = Array.from(containerNodeList);
-    const taskIndex = containerArray.indexOf(task);
+    const taskIndex = containerArray.indexOf(taskContainer);
     tasks.splice(taskIndex, 1);
-    task.remove();
-
-    // localStorage.setItem("my-todo", JSON.stringify(tasks));
+    taskContainer.remove();
     setPersistent(tasks);
     counter.textContent = tasks.length;
   }
