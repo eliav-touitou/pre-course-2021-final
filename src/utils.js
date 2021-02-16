44d4c70cb6fbf5ddb84df7f272891025e4d7b5c4
eliav-touitou <77244738+eliav-touitou@users.noreply.github.com>
2021-02-16 19:19:43 +0200

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,4 @@
-const API_KEY = ""; // Assign this variable to your JSONBIN.io API key if you choose to use it.
+const API_KEY = "$2b$10$CZt7ltqtf7n5YtsT097M4.AGaB58sIoKEafkw22vKIcNxTystSMmu"; // Assign this variable to your JSONBIN.io API key if you choose to use it.
 const DB_NAME = "my-todo";
 const url = "https://api.jsonbin.io/v3/b/601860c25415b40ac220fd6f";
 
@@ -25,19 +25,26 @@ function getPersistent() {
 
 // Saves the given data into persistent storage by the given key.
 // Returns 'true' on success.
-function setPersistent(data) {
-  const dataObject = {
-    "my-todo": data,
-  };
+function setPersistent(key, data) {
+  const sendObject = {};
+  sendObject[key] = data;
+
+  const dataString = JSON.stringify(sendObject);
   const init = {
     method: "PUT",
+    body: dataString,
     headers: {
       "Content-Type": "application/json",
+      "X-Master-Key": API_KEY,
+      "X-Bin-Versioning": false,
     },
-    body: JSON.stringify(dataObject),
   };
   const request = new Request(url, init);
+  const loader = document.querySelector(".loader");
+  loader.style.visibility = "visible";
+
   return fetch(request).then((response) => {
+    loader.style.visibility = "hidden";
     return response.ok;
   });
 }
