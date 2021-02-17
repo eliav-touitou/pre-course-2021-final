89bfb85c09b0418daae08ef6d59ffd21b3fa85af
eliav-touitou <77244738+eliav-touitou@users.noreply.github.com>
2021-02-17 21:33:33 +0200

    adding post and put

--- backEnd/index.js
+++ backEnd/index.js
@@ -3,14 +3,43 @@ const binId = require("uuid");
 const app = express();
 const fs = require("fs");
 app.use(express.json());
-const PORT = 3000;
 
-app.get("/b", (req, res) => {});
+app.get("/v3/b/:id", (req, res) => {
+  const id = req.params.id;
+  try {
+    const binContent = fs.readFileSync(`./bins/${id}.json`);
+    res.send(binContent);
+  } catch (e) {
+    res.status(422).json({ message: "Invalid Record ID" });
+  }
+});
 
-app.get("/b:id", (req, res) => {});
+app.put("/v3/b/:id", (req, res) => {
+  const body = req.body;
+  const id = req.params.id;
+  const binExist = fs.existsSync("./bins/${id}.json");
 
-app.put("/b:id", (req, res) => {});
+  if (!binExist) {
+    res.status(404).json({
+      message: "Bin not found",
+      success: false,
+    });
+    return;
+  }
+  fs.writeFileSync(`./bins/${id}.json`, JSON.stringify(body, null, 4));
+  const successMessage = {
+    success: true,
+    data: body,
+
+    version: 1,
+    parentId: id,
+  };
+  res.send(successMessage);
+});
 
 app.post("/b", (req, res) => {});
 
 app.delete("/b:id", (req, res) => {});
+
+app.listen(3000);
+console.log(`listening to 3000`);
